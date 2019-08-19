3d452419c9dcedfd00461030e45b160ad6baa386
wh75er <wh75er@gmail.com>
2019-08-19 12:33:50 +0300

    initialize post gathering function for posts

--- export_posts.py
+++ export_posts.py
@@ -1,10 +1,37 @@
 from collections import namedtuple
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+
+only_a_tags_with_class_c5 = SoupStrainer("div", class_="v1Nh3 kIKUG")
+Post = namedtuple("Post", ["credits", "pics"])
+
+def gatherProfilePosts(url):
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        return None
+
+    page_source = r.text
+    soup = BeautifulSoup(page_source, "html.parser", parse_only=only_a_tags_with_class_c5)
+    for a in soup:
+        urls.append(a["href"])
+
+    return "hello world"
 
 def exportPosts(urls_filename):
-    Post = namedtuple("Post", ["credits", "pics"])
     posts = [Post("@josselin", [1, 2, 3]), Post("@dreammagazine\n@josselin", [4])]
+
+    f = open(urls_filename)
+
+    for url in f:
+        url = url[:-1]
+        profilePosts = gatherProfilePosts(url)
+
+    f.close()
     
     return posts
 
 if __name__ == "__main__":
-    print()
+    posts = exportPosts("blabla")
+    for post in posts:
+        print(post)
