b6f9da3ab920281a8bee866f7233acf2e4a25ed4
wh75er <wh75er@gmail.com>
2019-08-20 12:31:45 +0300

    change structure of run script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo >> ../logs
-date >> ../logs
-python main.py >> ../logs
+path=/home/wh75er/projects/inst2vk
+
+echo >> $path/logs
+date >> $path/logs
+python $path/inst2vk/main.py >> $path/logs
+
